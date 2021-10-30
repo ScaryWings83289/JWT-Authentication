ffb633f62e71464ab5dc7a0ce262e81c746af17c
DIVYANKAR BHARGAV <divyankarbhargav@gmail.com>
2021-10-30 20:36:34 +0530

    remove cors error2

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const auth = require('./helpers/jwt.js');
 const unless = require('express-unless');
+const cors = require('cors');
 const users = require('./controllers/User.js');
 
 if (process.env.NODE_ENV !== "production") {
@@ -10,17 +11,24 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 // CORS options
-app.use((req, res, next) => {
-  //allow access from every, elminate CORS
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.removeHeader('x-powered-by');
-  //set the allowed HTTP methods to be requested
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  //headers clients can use in their requests
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+//app.use((req, res, next) => {
+//  //allow access from every, elminate CORS
+//  res.setHeader('Access-Control-Allow-Origin', '*');
+//  res.removeHeader('x-powered-by');
+//  //set the allowed HTTP methods to be requested
+//  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//  //headers clients can use in their requests
+//  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//  res.setHeader('Access-Control-Allow-Credentials', true);
+//  next();
+//});
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200, // For legacy browser support
+  methods: "GET, PUT, POST, DELETE"
+}
+
+app.use(cors(corsOptions));
 
 // middleware for authenticating token submitted with requests
 auth.authenticateToken.unless = unless;
@@ -45,7 +53,7 @@ mongoose
   .catch(err => console.log(err));
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
