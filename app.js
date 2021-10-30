51a415c9e8547eb59131eff496a4d530f1ee6c6e
DIVYANKAR BHARGAV <divyankarbhargav@gmail.com>
2021-10-30 18:56:58 +0530

    Removed error handler

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const auth = require('./helpers/jwt.js')
-const unless = require('express-unless')
-const users = require('./controllers/User.js')
-const errors = require('./helpers/errorHandler.js')
+const auth = require('./helpers/jwt.js');
+const unless = require('express-unless');
+const users = require('./controllers/User.js');
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
 // middleware for authenticating token submitted with requests
-auth.authenticateToken.unless = unless
+auth.authenticateToken.unless = unless;
 app.use(auth.authenticateToken.unless({
   path: [
     { url: '/users/login', methods: ['POST'] },
     { url: '/users/register', methods: ['POST'] }
   ]
-}))
+}));
 
-app.use(express.json()) // middleware for parsing application/json
-app.use('/users', users) // middleware for listening to routes
-app.use(errors.errorHandler); // middleware for error responses
+app.use(express.json()); // middleware for parsing application/json
+app.use('/users', users); // middleware for listening to routes
 
 
 // MONGODB Configuration
